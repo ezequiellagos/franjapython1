801564f4049aaa75c9cd0ecabe6f03ba5c970172
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2022-04-13 17:07:10 -0400

    Operadores aritmeticos y variables

---
+++ 1.- Operadores aritemeticos.py
@@ -0,0 +1,23 @@
+print("Suma")
+print(2 + 3)
+
+print("Resta")
+print(5 - 3)
+
+print("Multiplicación")
+print(5 * 3)
+
+print("División")
+print(21 / 5)
+
+print("División entera")
+print(21 // 5)
+
+print("División módulo")
+print(21 % 5)
+
+print("Exponente")
+print(2 ** 3)
+
+# Reglas de procedencia
+print(5 + (5 * 2))
---
+++ 2.- Variables.py
@@ -0,0 +1,11 @@
+numero = 3
+print(numero)
+
+numero1 = 5
+numero2 = 3
+print(numero1 + numero2)
+
+numero = numero1 ** numero2
+print(numero)
+
+numero_resultante = numero1 ** numero2
