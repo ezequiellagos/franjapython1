331c43416efc6dcf2e01802030429fdf2e6c88e5
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2022-06-29 15:01:43 -0400

    clase 29 de junio parte 1

--- 23.- Manejo de archivos.py
+++ 23.- Manejo de archivos.py
@@ -2,6 +2,14 @@
 
 # with: manejador contextual.Si el programa se cierra inesperadamente, evita que se corrompa el archivo
 
+# Modos de apertura
+# r  Solo lectura
+# r+ Lectura y escritura
+# w  Solo escritura. Sobrescribe el archivo si existe, lo crea si no existe
+# w+ Escritura y lectura. Sobrescribe el archivo si existe, lo crea si no existe
+# a  Añadido (agrega contenido). Crea el archivo si no existe
+# a+ Añadido (agrega contenido) y lectura. Crea el archivo si no existe
+
 def leer():
     numeros = []
     with open('./archivos/numeros.txt', 'r') as file:
@@ -11,10 +19,14 @@ def leer():
     print( numeros )
 
 def escribir():
-    pass
+    nombres = ['Ezequiel', 'Juan', 'Rocío', 'David']
+    with open('./archivos/nombres.txt', 'a', encoding="utf-8") as file:
+        for nombre in nombres:
+            file.write(nombre)
+            file.write("\n")
 
 def main():
-    leer()
+    escribir()
 
 if __name__ == '__main__':
     main()
---
+++ 24.- Ejercicio 1.py
@@ -0,0 +1,2 @@
+# Ejercicio 1
+# Escriba una función que aleatoriamente escoja un número entero entre 1 y 10 y lo guarde en un fichero.
